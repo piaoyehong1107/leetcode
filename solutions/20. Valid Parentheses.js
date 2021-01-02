4ea727fcd391c4fdd663e5032504f71a033e8fea
Yehong Piao <67206322+piaoyehong1107@users.noreply.github.com>
2021-01-02 16:30:04 -0600

    N/A

--- solutions/20. Valid Parentheses.js
+++ solutions/20. Valid Parentheses.js
@@ -9,16 +9,22 @@ var isValid = function(s) {
         '[': ']'
     }
     let arr=s.split("")
-    // console.log(hash[arr[5]])
-    for(i=0;i<arr.length;i++){
+​
+//     console.log(arr.splice(0,1))
+    let i=0
+    while(i<arr.length){
         if(hash[arr[i]] && arr.includes(hash[arr[i]])){
             if((arr.indexOf(hash[arr[i]])-i)%2 !==0){
-                return true
+                arr.splice(arr.indexOf(hash[arr[i]]),1)
+                arr.splice(i,1)
+                console.log(arr)
             }else{
                 return false
             }
         }else{
             return false
         }
-    }
-};
+        i++
+    return true
+    };
+}
