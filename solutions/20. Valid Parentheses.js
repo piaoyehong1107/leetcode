21ff271ae08574e105d9385094ea64d653372128
Yehong Piao <piaoyehong1107@gmail.com>
2021-01-06 23:11:34 -0600

    N/A

--- solutions/20. Valid Parentheses.js
+++ solutions/20. Valid Parentheses.js
@@ -3,25 +3,43 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    let hash={
-        '(': ')', 
-        '{': '}', 
-        '[': ']'
-    }
-    let arr=s.split("")
+    let hash='(){}[]'
+    let stack=[]
+    // console.log(hash.indexOf("{"))
+    // console.log(s[1])
     let i=0
-    while(i<arr.length){
-        if(hash[arr[i]] && arr.includes(hash[arr[i]])){
-            if((arr.indexOf(hash[arr[i]])-i)%2 !==0){
-                arr.splice(arr.indexOf(hash[arr[i]]),1)
-                arr.splice(i,1)
-                console.log(arr)
-            }else{
-                return false
-            }
+    while(i<s.length){
+        
+        
+        
+        if (hash.indexOf(s[i]) %2 == 0){
+            console.log(stack)
+            stack.push(s[i])
+            i++
+        }else if(hash.indexOf(s[i]) %2 !== 0 && (hash.indexOf(stack.pop())+1 == hash.indexOf(s[i])){
+                 i++
         }else{
-            return false
-        }
-    return true
-    };
+                 return false
+            }
+    }
+    return stack.length ==0
 }
+​
+// const isValid = (str) => {
+//     if (str.length === 1) return false;
+    
+//     const pairs = "(){}[]";
+//     const stack = [];
+//     for(const bracket of str) {
+//         const index = pairs.indexOf(bracket);
+//         if (index % 2 === 0) { // is opening bracket
+//             stack.push(index);
+//         } else {
+//             if (stack.pop() !== index - 1) {
+//                 return false;
+//             }
+//         }
+//     }
+    
+//     return stack.length === 0;
+// }
