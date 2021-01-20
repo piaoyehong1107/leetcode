15c7777aaa2501ba2a37c48b3f8577302755403a
Yehong Piao <piaoyehong1107@gmail.com>
2021-01-19 23:29:00 -0600

    84 ms

--- solutions/415. Add Strings.js
+++ solutions/415. Add Strings.js
@@ -1,35 +1,33 @@
-/**
- * @param {string} num1
- * @param {string} num2
- * @return {string}
- */
-var addStrings = function(num1, num2) {
-    num1a=num1.split("").reverse()
-    num2a=num2.split("").reverse()
-    let num1f=0
-    let num2f=0
+        undefined: 0,
+        '0': 0,
+        '1': 1,
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5,
+        '6': 6,
+        '7': 7,
+        '8': 8,
+        '9': 9
+    }
     let i=0
-    let j=0
-    while(i<num1a.length){
-        num1f=num1f+parseInt(num1a[i]) * Math.pow(10, i)
+    let adv=0
+    let sum=''
+    while(num1a[i]||num2a[i]){
+        let temp=dict[num1a[i]]+dict[num2a[i]]+adv
+        if(temp>9){
+            adv=1
+            sum=(temp%10).toString() + sum
+        }
+        else{
+            adv=0
+            sum=temp.toString() + sum
+        }
         i++
     }
-    console.log(num1f)
-    while(j<num2a.length){
-        num2f=num2f+parseInt(num2a[j]) * Math.pow(10, j)
-        j++
-    }
-     console.log(num2f)
-    return (num1f+num2f).toString()
+    if(adv==1){
+        return adv.toString()+sum
+    }else{
+        return sum
+    }
 };
-​
-// addStrings = function(num1, num2) {
-//     const dict = {
-//         '' : 0,
-//         '0': 0,
-//         '1': 1,
-//         '2': 2,
-//         '3': 3,
-//         '4': 4,
-//         '5': 5,
-//         '6': 6,
