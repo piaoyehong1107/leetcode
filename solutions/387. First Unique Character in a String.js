70a2749da1f971bdfd3957aac00b9f633eb085d3
Yehong Piao <piaoyehong1107@gmail.com>
2021-01-25 23:32:07 -0600

    N/A

---
+++ solutions/387. First Unique Character in a String.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+ // console.log(s.splice(1,2))
+    let i=0
+    if(s.length==0) return -1
+    else if(s.length==1) return 0
+    else if(s.length==2 && s[0]==s[1]){
+        return -1
+    }
+    while(i < s.length-1){
+        const k=s.split("")
+        k.splice(i,1)
+        // console.log(k)
+        if(!k.includes(s[i])){
+            return i
+        }  
+        i++
+        if(i==s.length-2) return -1
+    }
+};
