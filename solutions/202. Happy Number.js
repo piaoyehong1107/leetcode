5d370a4032188a9c9bdb92a92a302fdcf118204b
Yehong Piao <piaoyehong1107@gmail.com>
2021-01-29 00:01:32 -0600

    N/A

--- solutions/202. Happy Number.js
+++ solutions/202. Happy Number.js
@@ -4,16 +4,16 @@
  */
 var isHappy = function(n) {
     let st=n.toString().split("")
-    let final=0 
+    let final=0
     for(let i=0; i< st.length; i++){
         final=final+Math.pow(parseInt(st[i]),2)
-        console.log(final)
     }
-    if(final.toString().length==1 && final==1) {
-        return true
-    }else if(final.toString().length==1 && final !=1){
-        return false
-    }
-    isHappy(parseInt(final))
+    if(final.toString().length !=1 ) {
+        isHappy(parseInt(final))
+     }
+    console.log(final)
+    if(final !=1 ) return false
+    if(final ==1) return true
+    
     return final
 };
