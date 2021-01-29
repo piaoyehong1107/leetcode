fa57f3f6e059fccabcd0dc1bc603cc5f51d59abb
Yehong Piao <piaoyehong1107@gmail.com>
2021-01-28 23:34:47 -0600

    N/A

--- solutions/202. Happy Number.js
+++ solutions/202. Happy Number.js
@@ -5,14 +5,15 @@
 var isHappy = function(n) {
     let st=n.toString().split("")
     let final=0 
-    for(let i=0; i<st.length; i++){
+    for(let i=0; i< st.length; i++){
         final=final+Math.pow(parseInt(st[i]),2)
         console.log(final)
     }
-    if(final==1) {
+    if(final.toString().length==1 && final==1) {
         return true
+    }else if(final.toString().length==1 && final !=1){
+        return false
     }
-    if(final.toString().length==1 && final!=1){return false}
     isHappy(parseInt(final))
     return final
 };
