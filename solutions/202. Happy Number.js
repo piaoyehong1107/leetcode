aca80bf7e53551d8f3ac40cb5ba278bbe05de8c2
Yehong Piao <piaoyehong1107@gmail.com>
2021-02-01 23:12:02 -0600

    N/A

--- solutions/202. Happy Number.js
+++ solutions/202. Happy Number.js
@@ -5,21 +5,21 @@
 var isHappy = function(n) {
     let temp=[]
     let sum
-      var isHappy2 = function(n){
-        n=n.toString().split("")
-        sum=0
-        for(let i=0; i<n.length; i++){
-          sum=sum+Math.pow(n[i],2)
-        }
-        temp.push(sum)
-        console.log(temp)
-        while(sum!=1){
-          isHappy2(sum)
-            if(temp.includes(sum)){console.log("Y")}
-        }
-        return sum
-      }
-isHappy2(n)
-return sum == 1
+          var isHappy2 = function(n){
+            n=n.toString().split("")
+            sum=0
+                for(let i=0; i<n.length; i++){
+                  sum=sum+Math.pow(n[i],2)
+                }
+                if(sum==1) return
+                if(!temp.includes(sum)){
+                    temp.push(sum)
+                    console.log(temp)
+                    isHappy2(sum)
+                }
+            return sum
+          }
+    isHappy2(n)
+    return sum == 1
 };
 ​
