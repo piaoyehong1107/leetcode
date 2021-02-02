19769ae17bcce4110af6300d817ddebcf87766ce
Yehong Piao <piaoyehong1107@gmail.com>
2021-02-01 22:45:07 -0600

    N/A

--- solutions/202. Happy Number.js
+++ solutions/202. Happy Number.js
@@ -3,19 +3,23 @@
  * @return {boolean}
  */
 var isHappy = function(n) {
-    let st=n.toString().split("")
-    let final=0
-    for(let i=0; i< st.length; i++){
-        final=final+Math.pow(parseInt(st[i]),2)
-    }
-    console.log(final)
-    if(final.toString().length !=1 ) {
-        isHappy(parseInt(final))
-     }
-    if(final.toString().length ==1 && final ==1){
-        return true
-    }else if(final.toString().length ==1 && final !=1){
-        return false
-    }
-    return final
+    let temp=[]
+    let sum
+      var isHappy2 = function(n){
+        n=n.toString().split("")
+        sum=0
+        for(let i=0; i<n.length; i++){
+          sum=sum+Math.pow(n[i],2)
+        }
+        temp.push(sum)
+        console.log(temp)
+        while(sum!=1){
+          isHappy2(sum)
+            if(temp.includes(sum)){console.log("Y")}
+        }
+        return sum
+      }
+isHappy2(n)
+return sum == 1
 };
+​
