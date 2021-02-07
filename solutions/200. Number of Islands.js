a63ecce5f6be60400fafc3c956ae651a1f037575
Yehong Piao <piaoyehong1107@gmail.com>
2021-02-07 17:24:00 -0600

    120 ms

--- solutions/200. Number of Islands.js
+++ solutions/200. Number of Islands.js
@@ -16,23 +16,34 @@ var numIslands = function(grid) {
     }
     grid.push(arr)
     grid.unshift(arr)
-    // console.log(grid)
+    
+    var check = function(ele,i,j){
+        if(ele[i-1][j]=="1"){
+            ele[i-1][j]="0"
+            check(ele,i-1,j)
+        }
+        if(ele[i+1][j]=="1"){
+            ele[i+1][j]="0"
+            check(ele,i+1,j)
+        }
+        if(ele[i][j+1]=="1"){
+            ele[i][j+1]="0"
+            check(ele,i,j+1)
+        }
+        if(ele[i][j-1]=="1"){
+            ele[i][j-1]="0"
+            check(ele,i,j-1) 
+        }
+    }
     for(let i=1; i<m+1; i++){
         for(let j=1; j<n+1; j++){
             let temp=[]
             if(grid[i][j]=="1"){
-               temp.push(grid[i-1][j])
-                temp.push(grid[i+1][j])
-                temp.push(grid[i][j+1])
-                temp.push(grid[i][j-1])
-                console.log(temp) 
-                let temp2=[]
-                for(let k=0; k<temp.length; k++){
-                    if(temp[k]=="1"){temp2.push(temp[k])}
-                }
-                if(temp2.length<=1){final++}
+                final++
+                check(grid,i,j)
             }
         }
     }
+    console.log(grid)
     return final
 };
