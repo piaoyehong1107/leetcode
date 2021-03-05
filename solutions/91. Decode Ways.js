de50e131d8a3a8041b62b614f0c7bd744a744cde
Yehong Piao <piaoyehong1107@gmail.com>
2021-03-04 23:10:49 -0600

    N/A

--- solutions/91. Decode Ways.js
+++ solutions/91. Decode Ways.js
@@ -2,18 +2,29 @@
  * @param {string} s
  * @return {number}
  */
-var numDecodings = function(s) {
-   const dic={
-        "1": "A","2": "B","3": "C","4": "D","5": "E","6": "F","7": "G","8": "H","9": "I","10": "J","11": "K","12": "L","13": "M",
-        "14":"N","15":"O","16":"P","17":"Q","18":"R","19":"S","20":"T","21":"U","22":"V","23":"W","24":"X","25":"Y","26":"Z",
-    }
-    const l=s.length
-    let final=1
-    for(let i=0; i<l; i++){
-        if(dic[s[i]] && dic[s[i+1]] && dic[s[i]+s[i+1]]){
-            final++
+function numDecodings(s) {
+  if (s == null || s.length === 0) return 0;
+  if (s[0] === '0') return 0;
+​
+      const dp = new Array(s.length + 1).fill(0);
+ 
+​
+      dp[0] = 1;
+      dp[1] = 1;
+ 
+      for (let i = 2; i <= s.length; i++) {
+        const a = Number(s.slice(i - 1, i));  // last one digit
+          console.log(a)
+        if (a >= 1 && a <= 9) {
+          dp[i] += dp[i - 1];
         }
-        console.log(i)
-    }
-    return final
-};
+        // console.log(dp)
+        const b = Number(s.slice(i - 2, i));  // last two digits
+        if (b >= 10 && b <= 26) {
+          dp[i] += dp[i - 2];
+        }
+          console.log(dp)
+      }
+​
+  return dp[s.length];
+}
